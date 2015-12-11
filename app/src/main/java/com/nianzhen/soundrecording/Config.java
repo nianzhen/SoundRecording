3365e25b862a633884de72762dd78e4f6a83965d
chengsheng <chengsheng@renrendai.com>
2015-12-11 17:09:56 +0800

    第一次提交

---
+++ app/src/main/java/com/nianzhen/soundrecording/Config.java
@@ -0,0 +1,9 @@
+package com.nianzhen.soundrecording;
+
+/**
+ * Created by Administrator on 2015/12/10.
+ */
+public class Config {
+    public static boolean DEBUG = true;
+    public static String NOTE = "note";
+}
